c4c0c0e4cb5786cde98bbac24b25b9a917997a84
Atishay25 <atishayjain2552@gmail.com>
2023-03-05 22:25:05 +0530

    test script

---
+++ test.py
@@ -0,0 +1,53 @@
+import subprocess
+import os
+import sys
+
+# test sat and unsat for n in range [1,n_range] and T in range [0,t_range]
+# arguement is the file to be tested 
+t_range = 5
+n_range = 5
+
+testfile = sys.argv[1]
+
+for i in range(1,n_range+1):
+    for j in range(t_range+1):
+        for k in range(2):
+            sat_unsat = ""
+            if k == 0:
+                sat_unsat = "sat"
+            else:
+                sat_unsat = "unsat"
+            data, temp = os.pipe()
+            os.write(temp, bytes("5 10\n", "utf-8"));
+            os.close(temp)
+            s1 = subprocess.check_output("python3 generator.py " + str(i) + " " + str(j) +  " " + sat_unsat + " test.txt > correct.txt", stdin = data, shell = True)
+            if k == 0:
+                s2 = subprocess.check_output("time python3 " + testfile + " test.txt > my.txt", stdin = data, shell = True)
+                s3 = subprocess.check_output("python3 verifier.py test.txt my.txt", stdin = data, shell = True)
+                time = s2.decode("utf-8")
+                verification = s3.decode("utf-8")
+                with open('test_output' +'.txt', 'a') as op:
+                    op.write('__________________________________________________________________')
+                    op.write("TEST FOR sat n = " + str(i) + " T = " + str(j) + "\n")
+                    op.write(time)
+                    op.write('\n')
+                    op.write(verification)
+                    op.write('\n')
+                    op.write('__________________________________________________________________')
+                    op.write('\n')
+            else:
+                s2 = subprocess.check_output("time python3 " + testfile + " test.txt > my.txt", stdin = data, shell = True)
+                s3 = subprocess.check_output("time python3 " + testfile + " test.txt", stdin = data, shell = True)
+                time = s2.decode("utf-8")
+                unsat = s3.decode("utf-8")
+                with open('test_output' +'.txt', 'a') as op:
+                    op.write('__________________________________________________________________')
+                    op.write("TEST FOR unsat n = " + str(i) + " T = " + str(j) + "\n")
+                    op.write(time)
+                    op.write('\n')
+                    if unsat != "unsat":
+                        op.write("INCORRECT\n")
+                    op.write(unsat)
+                    op.write('\n')
+                    op.write('__________________________________________________________________')
+                    op.write('\n')
