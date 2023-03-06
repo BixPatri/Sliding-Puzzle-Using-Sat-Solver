99d2660edfb97eeafff582f0ae97919d9d608946
Atishay25 <atishayjain2552@gmail.com>
2023-03-06 18:54:40 +0530

    Final sat solver

--- 210050009_210050015_210050026_tile_loop.py
+++ 210050009_210050015_210050026_tile_loop.py
@@ -1,4 +1,5 @@
 from z3 import *
+import math
 import sys
 
 file = sys.argv[1]
@@ -8,95 +9,141 @@ with open(file) as f:
 	matrix = []
 	for line in f:
 		matrix.append([int(x) for x in line.split()])
+# print(matrix)
 
-## initialising the list of variables present.
+
+def equal(a,b):
+    k=[elem1==elem2 for elem1, elem2 in zip(a, b)]
+    return And(k)
+
+# Initialising the list of variables present.
 def initialize_variables(n, t):
     # Create variables
-    variables = {}
-    for i in range(n):
-        for j in range(n):
-            for k in range(t+1):
-                var_name = "v{}{}{}".format(i, j, k)
-                variables[var_name] = Int(var_name)
+    n_b=math.ceil(2*math.log(n,2))
+    # variables =  [[[None for _ in range(t+1)] for _ in range(n)] for _ in range(n)]
 
+    variables=[[[[Bool("v[{}][{}][{}][{}]".format(i, j, k,l)) for l in range(n_b)] for k in range(t+1)] for j in range(n)] for i in range(n)]                    
+    # print(variables)
     return variables
 
-vars = initialize_variables(n,T)
-s = Solver()
-
-for i in range(n):
-        for j in range(n):
-            s.add(vars["v{}{}{}".format(i, j,T)] == n*i + j +1)
-
-for i in range(n):
-        for j in range(n):
-	        s.add(Int("v{}{}{}".format(i,j,0)) == matrix[i][j])
+# Add constraints for initial and final states
+def initial_final_constraints(solver, vars, n):
+    n_b=math.ceil(2*math.log(n,2))
     
-     
+    # Add the final constraint
+    for i in range(n):
+        for j in range(n):
+            k=n*i+j+1
+            for l in range(n_b):
+                if k%2:
+                    solver.add(vars[i][j][T][l])
+                else:
+                    solver.add(Not(vars[i][j][T][l]))
+                k=k//2
+            
+    # Add the initial constraint
+    for i in range(n):
+        for j in range(n):
+            k=matrix[i][j]
+            for l in range(n_b):
+                if k%2:
+                    solver.add(vars[i][j][0][l])
+                else:
+                    solver.add(Not(vars[i][j][0][l]))
+                k=k//2
+    
+# Add the constraints for shifts
 def add_shift_constraints(solver, variables, n, t):
+    
+    # Clauses corresponding to the shifts
+    clause =  [[[None for _ in range(t)] for _ in range(n)] for _ in range(4)]
+
     for k in range(1,t+1):
+
         # Add right shift constraints for rows
-        clause = []
         for i in range(n):
                 # Roll-over shift for row i
-                roll_over_clause = [variables["v{}{}{}".format(i, j, k)] == variables["v{}{}{}".format(i, j-1, k-1)] for j in range(1, n)]
-                roll_over_clause.append(variables["v{}0{}".format(i, k)] == variables["v{}{}{}".format(i, n-1, k-1)])
-                clause.append(And(roll_over_clause))
+                roll_over_clause = [equal(variables[i][j][k] ,variables[i][ j-1 ][k-1]) for j in range(1, n)]
+                roll_over_clause.append(equal(variables[i][0][k], variables[i][n-1][k-1]))
+                others = [equal(variables[l][ j][ k],variables[l][ j][ k-1]) for l in range(n) for j in range(n) if l!= i]
+                clause[0][i][k-1] = And(roll_over_clause + others)
 
         # Add left shift constraints for rows
         for i in range(n):
                 # Roll-over shift for row i
-                roll_over_clause = [variables["v{}{}{}".format(i, j-1, k)] == variables["v{}{}{}".format(i, j, k-1)] for j in range(1, n)]
-                roll_over_clause.append(variables["v{}{}{}".format(i, n-1,k)] == variables["v{}{}{}".format(i, 0, k-1)])
-                clause.append(And(roll_over_clause))
+                roll_over_clause = [equal(variables[i][j-1][ k], variables[i][j][k-1]) for j in range(1, n)]
+                roll_over_clause.append(equal(variables[i][ n-1][k] ,variables[i][0][k-1]))
+                others = [equal(variables[l][ j][ k] , variables[l][j][ k-1]) for l in range(n) for j in range(n) if l!= i]
+                clause[1][i][k-1]= And(roll_over_clause + others)
 
         # Add Down shift constraints for columns
         for j in range(n):
                 # Roll-over shift for column j
-                roll_over_clause = [variables["v{}{}{}".format(i, j, k)] == variables["v{}{}{}".format(i-1, j, k-1)] for i in range(1, n)]
-                roll_over_clause.append(variables["v0{}{}".format(j, k)] == variables["v{}{}{}".format(n-1, j, k-1)])
-                clause.append(And(roll_over_clause))
+                roll_over_clause = [equal(variables[i][j][k] , variables[i-1][j][ k-1]) for i in range(1, n)]
+                roll_over_clause.append(equal(variables[0][j][k] , variables[n-1][ j][ k-1]))
+                others = [equal(variables[i][l][k] , variables[i][l][k-1]) for i in range(n) for l in range(n) if l!= j]
+                clause[2][j][k-1] = And(roll_over_clause + others)
 
         # Add Up shift constraints for columns
         for j in range(n):
                 # Roll-over shift for column j
-                roll_over_clause = [variables["v{}{}{}".format(i-1, j, k)] == variables["v{}{}{}".format(i, j, k-1)] for i in range(1, n)]
-                roll_over_clause.append(variables["v{}{}{}".format(n-1,j, k)] == variables["v{}{}{}".format(0, j, k-1)])
-                clause.append(And(roll_over_clause))
+                roll_over_clause = [equal(variables[i-1][j][k] ,variables[i] [j] [k-1]) for i in range(1, n)]
+                roll_over_clause.append(equal(variables[n-1][j][k] , variables[0][j][k-1]))
+                others = [equal(variables[i][l][k] , variables[i][l][k-1]) for i in range(n) for l in range(n) if l!= j]
+                clause[3][j][k-1] = And(roll_over_clause + others)
         
-        prev_state_clause = []
-        
-        # Add the constraint that the row is same as the previous step
-        row_shift = []
+        # boolean list to check whether the previous entry same as the current
+        equal_bools = [] 
+
+        # Add the constraint that the configuration is same as the previous step
         for i in range(n):
-            curr_row_shift = []
             for j in range(n):
-                curr_row_shift.append(variables["v{}{}{}".format(i,j,k)] == variables["v{}{}{}".format(i, j, k-1)])
-            row_shift.append(And(curr_row_shift))
-
-        prev_state_clause.append(Sum([z3.If(row_shift[p],0,1) for p in range(n)]) <= 1)
+                equal_bools.append(equal(variables[i][j][k],variables[i][j][k-1]))
         
-        # Add the constraint that the col is same as the previous step
-        col_shift = []
-        for j in range(n):
-            curr_col_shift = []
-            for i in range(n):
-                curr_col_shift.append(variables["v{}{}{}".format(i,j,k)] == variables["v{}{}{}".format(i, j, k-1)])
-            col_shift.append(And(curr_col_shift))
+        # Clause that the previous state is exactly same as current
+        prev_state_clause = And(equal_bools)
 
-        prev_state_clause.append(Sum([z3.If(col_shift[p],0,1) for p in range(n)]) <= 1)
+        # Clause that either a move happens or it is same as the current state.
+        solver.add(Or(Or([clause[i//n][i%n][k-1] for i in range(4*n)]),prev_state_clause))
+    
+    # Adding the clause to remove the redundant steps
+    for k in range(1,t):
+            solver.add(And([Or(Not(clause[0][i][k-1]),Not(clause[1][i][k])) for i in range(n)]))
+            solver.add(And([Or(Not(clause[1][i][k-1]),Not(clause[0][i][k])) for i in range(n)]))
+            solver.add(And([Or(Not(clause[2][i][k-1]),Not(clause[3][i][k])) for i in range(n)]))
+            solver.add(And([Or(Not(clause[3][i][k-1]),Not(clause[2][i][k])) for i in range(n)]))
+    return clause
 
-        # If any row changes then the other rows remain the same same with the columns.
-        solver.add(Or(prev_state_clause))
+vars = initialize_variables(n,T)
 
-        # Add constraint to ensure Atmost one shift per time step
-        solver.add(Sum([z3.If(clause[i], 1, 0) for i in range(4*n)]) <= 1)
+s = Solver()
 
-add_shift_constraints(s,vars,n,T)
+initial_final_constraints(s,vars,n)
+
+assignments = add_shift_constraints(s,vars,n,T)
+
 x = s.check()
-print(x)
-print(s)
+
+if n==1:
+    print("sat")
+    exit()
+
 if x == sat:
-	m = s.model()
-print(m)
-        
+  m = s.model()
+  print("sat")
+  for k in range(1,T+1):
+       for j in range(n):
+           if m.eval(assignments[0][j][k-1]):
+               print("{}r".format(j))
+               break
+           if m.eval(assignments[1][j][k-1]):
+               print("{}l".format(j))
+               break
+           if m.eval(assignments[2][j][k-1]):
+               print("{}d".format(j))
+               break
+           if m.eval(assignments[3][j][k-1]):
+               print("{}u".format(j))
+               break
+else: 
+  print("unsat")
